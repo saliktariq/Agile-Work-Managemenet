6db18753d4094db769c03210854ba3186917b649
salik <saliktariq@icloud.com>
2023-04-08 05:03:59 +0100

    moved application to Azure cloud
    moved data layer of the application to Azure cloud

--- Agile-Work-Management/credentials.h
+++ Agile-Work-Management/credentials.h
@@ -4,9 +4,17 @@
 #include <string>
 
 namespace Credentials {
-    const std::string LOCALHOST = "127.0.0.1:3306";
-    const std::string USERNAME = "root";
-    const std::string PASSWORD = "rs232rj11";
+
+    // Local Database Credentials (Cache)
+    //const std::string LOCALHOST = "127.0.0.1:3306";
+    //const std::string USERNAME = "root";
+    //const std::string PASSWORD = "rs232rj11";
+    //const std::string DATABASE_NAME = "kanban_agile_work_management";
+
+    // Azure Cloud Credentials
+    const std::string LOCALHOST = "cpp-backend.mysql.database.azure.com:3306";
+    const std::string USERNAME = "salik_azure_sql_db";
+    const std::string PASSWORD = "rh32-b@$$-vxhu";
     const std::string DATABASE_NAME = "kanban_agile_work_management";
 }
 
